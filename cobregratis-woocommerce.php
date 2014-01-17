3d9c91a07acba37e492f6ee4d840be96172d7b87
claudiosmweb <contato@claudiosmweb.com>
2014-01-17 16:48:30 -0200

    added a base functions to initialize the gateway in woocommerce

--- cobregratis-woocommerce.php
+++ cobregratis-woocommerce.php
@@ -20,3 +20,75 @@
  * Domain Path:       /languages
  * GitHub Plugin URI: https://github.com/BielSystems/cobregratis-woocommerce
  */
+
+/**
+ * WooCommerce is missing notice.
+ *
+ * @since  1.0.0
+ *
+ * @return string WooCommerce is missing notice.
+ */
+function wc_cobregratis_woocommerce_is_missing() {
+	echo '<div class="error"><p>' . sprintf( __( 'Cobre Gr&aacute;tis for WooCommerce depends on the last version of %s to work!', 'cobregratis-woocommerce' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', 'cobregratis-woocommerce' ) . '</a>' ) . '</p></div>';
+}
+
+/**
+ * Initialize the Cobre Grátis gateway.
+ *
+ * @since  1.0.0
+ *
+ * @return void
+ */
+function wc_cobregratis_gateway_init() {
+
+	// Checks with WooCommerce is installed.
+	if ( ! class_exists( 'WC_Payment_Gateway' ) ) {
+		add_action( 'admin_notices', 'wc_cobregratis_woocommerce_is_missing' );
+
+		return;
+	}
+
+	/**
+	 * Load textdomain.
+	 */
+	load_plugin_textdomain( 'cobregratis-woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	/**
+	 * Add the Cobre Grátis gateway to WooCommerce.
+	 *
+	 * @param  array $methods WooCommerce payment methods.
+	 *
+	 * @return array          Payment methods with Cobre Grátis.
+	 */
+	function wc_cobregratis_add_gateway( $methods ) {
+		$methods[] = 'WC_Cobregratis_Gateway';
+
+		return $methods;
+	}
+
+	add_filter( 'woocommerce_payment_gateways', 'wc_cobregratis_add_gateway' );
+
+	// Include the WC_Cobregratis_Gateway class.
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wc-cobregratis-gateway.php';
+}
+
+add_action( 'plugins_loaded', 'wc_cobregratis_gateway_init', 0 );
+
+/**
+ * Hides the Cobre Grátis with payment method with the customer lives outside Brazil.
+ *
+ * @param  array $available_gateways Default Available Gateways.
+ *
+ * @return array                     New Available Gateways.
+ */
+function wc_cobregratis_hides_when_is_outside_brazil( $available_gateways ) {
+
+	// Remove standard shipping option.
+	if ( isset( $_REQUEST['country'] ) && 'BR' != $_REQUEST['country'] ) {
+		unset( $available_gateways['cobregratis'] );
+	}
+
+	return $available_gateways;
+}
+
+add_filter( 'woocommerce_available_payment_gateways', 'wc_cobregratis_hides_when_is_outside_brazil' );

a99ff45a2bc1570adee386ee2104346f05c9cf85
claudiosmweb <contato@claudiosmweb.com>
2014-01-17 16:48:06 -0200

    created initial gateway class

---
+++ includes/class-wc-cobregratis-gateway.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Cobre Grátis for WooCommerce.
+ *
+ * @package   WC_Cobregratis
+ * @author    Claudio Sanches <contato@claudiosmweb.com>
+ * @license   GPL-2.0+
+ * @copyright 2013 BielSystems
+ */
+
+/**
+ * Cobra Grátis payment gateway class.
+ *
+ * @package WC_Cobregratis_Gateway
+ * @author  Claudio Sanches <contato@claudiosmweb.com>
+ * @since   1.0.0
+ */
+class WC_Cobregratis_Gateway extends WC_Payment_Gateway {
+
+}

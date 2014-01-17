4916f013a7c233890f8c8b84ede06fcf472fd4e4
claudiosmweb <contato@claudiosmweb.com>
2014-01-17 16:38:59 -0200

    created the plugin initial main file

---
+++ cobregratis-woocommerce.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cobre Grátis for WooCommerce.
+ *
+ * @package   WC_Cobregratis
+ * @author    Claudio Sanches <contato@claudiosmweb.com>
+ * @license   GPL-2.0+
+ * @copyright 2013 BielSystems
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Cobre Grátis for WooCommerce
+ * Plugin URI:        https://github.com/BielSystems/cobregratis-woocommerce
+ * Description:       Start getting money by bank billet in your checking account using Cobre Grátis
+ * Version:           1.0.0
+ * Author:            BielSystems, claudiosanches
+ * Author URI:        http://cobregratis.com.br/
+ * Text Domain:       cobregratis-woocommerce
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/BielSystems/cobregratis-woocommerce
+ */
